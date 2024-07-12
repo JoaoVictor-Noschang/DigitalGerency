7a27e2c070ccd93cc5f3d4ff83ad094441c95094
JoaoVictor-Noschang <joaovictornoschangsilva601@gmail.com>
2024-07-11 21:58:58 -0400

    Redoing the logic of batch registrations
    Redoing the logic of batch registrations, to be dynamic within JavaScript.

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -1,5 +1,5 @@
 const cadastros = [];
-let controleID = cadastros.length > 0 ? cadastros[cadastros.length - 1].id : 0;
+let controleID = 0;
 
 
 const codInput = document.querySelector("#codigo");
@@ -15,53 +15,58 @@ const actionButton = document.querySelector("#action-button");
 
 class Lote {
 
-    #id;
-    #lote;
-    #produto;
-    #fornecedor;
-    #codBarr;
-    #quantidade;
-    #vencimento;
-
     constructor(lote, prod, forn, codBarr, quant, venc) {
-        this.#id = ++cod;
-        this.#lote = lote;
-        this.#produto = prod;
-        this.#fornecedor = forn;
-        this.#codBarr = codBarr;
-        this.#quantidade = quant;
-        this.#vencimento = venc;
+        this.id = ++controleID;
+        this.lote = lote;
+        this.produto = prod;
+        this.fornecedor = forn;
+        this.codBarr = codBarr;
+        this.quantidade = quant;
+        this.vencimento = venc;
     }
 
-    get id() {
-        return this.#id;
+    static clear() {
+        codInput.value = '';
+        loteInput.value = '';
+        prodInput.value = '';
+        fornInput.value = '';
+        codBarrInput.value = "";
+        quantInput.value = '';
+        vencInput.value = '';
     }
 
-    get lote() {
-        return this.#lote;
+    
+    static cadastrarNovoLote() {
+
+        let loteValue = loteInput.value;
+        let prodValue = prodInput.value;
+        let fornValue = fornInput.value;
+        let codBarrValue = codBarrInput.value;
+        let quantValue = quantInput.value;
+        let vencValue = vencInput.value;
+
+        if (loteValue && prodValue && fornValue && codBarrValue && quantValue && vencValue) {
+            const newLote = new Lote(loteValue, prodValue, fornValue, codBarrValue, quantValue, vencValue);
+            cadastros.push(newLote);
+            console.log(cadastros);
+            Lote.clear();
+        } else {
+            alert("Todos os campos devem ser preenchidos!");
+        }
+
+
     }
 
-    get produto() {
-        return this.#produto;
-    }
-
-    get fornecedor() {
-        return this.#fornecedor;
-    }
-
-    get codBarr() {
-        return this.#codBarr;
-    }
-
-    get quantidade() {
-        return this.#quantidade;
-    }
-
-    get vencimento() {
-        return this.#vencimento;
-    }
 }
 
-let prod01 = new Lote("11-11-111-1111", "café", "tão bom", "789456123456123", 200, "2024-12-31");
+actionButton.addEventListener("click", () => {
+    Lote.cadastrarNovoLote();
+});
+
+/*
+let lot01 = new Lote("11-11-111-1111", "café", "tão bom", "789456123456123", 200, "2024-12-31");
+let lot02 = new Lote("22-22-222-2222", "chá", "Leão", "789456555555555", 130, "2024-11-25");
+cadastros.push(lot01, lot02);
+*/
 
 console.log(cadastros);
